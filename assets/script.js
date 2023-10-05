7619480dfe024d86efd653da557a39b3652ef870
Riham Aji <reham93189310@gmail.com>
2023-10-05 22:11:01 +0300

    INNOVATIVE QUALITIES & FEATURES SUBSCRIBE NOW & Footer Section

--- assets/script.js
+++ assets/script.js
@@ -40,3 +40,27 @@ function changeLang(a,b){
     console.log('Image source updated to', b);
 }
 /*--------------------------------------*/
+let items = document.querySelectorAll('.m-carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+/*------------------Carsoul data--------------------*/
+
+    fetch('https://dummyjson.com/products/category/smartphones')
+        .then(res => res.json())
+        .then(data =>console.log(data.products));
+      /*  .then(data =>console.log(data.products.title))
+        .then(data =>console.log(data.products.rating))
+        .then(data =>console.log(data.products.price))
+        .then(data =>console.log(data.products.images));*/
